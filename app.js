2f62121c0d9d9055058f5fe23b2e6ca2f72ead92
Israel Berlanga <israel0688@hotmail.com>
2020-05-12 20:05:15 -0500

    halfway done with 9.3.5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  const fs = require(`fs`);
+ /* const fs = require(`fs`);
   const generatePage = require('./src/page-template.js');
   
   const profileDataArgs = process.argv.slice(2);
@@ -13,4 +13,16 @@ fs.writeFile(`index.html`, generatePage(name, github), err =>
     if (err) throw new Error(err);
 
     console.log(`Portfolio complete! Check out index.html to see the output`);
-  });
+  }); */
+
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
